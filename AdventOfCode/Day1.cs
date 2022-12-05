4c256c6a19c502aef8bb1ea054b55035ae26639c
Lars Boerboom <lars@rebelit.nl>
2022-12-05 10:20:24 +0100

    day 1

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventOfCode
+{
+    public class Day1
+    {
+
+        /// <summary>
+        /// stupid first version 
+        /// </summary>
+
+        /*
+        int topCalories = 0;
+        int currentCalories = 0;
+        int index = 0;
+        public void Part1()
+        {
+            foreach (string line in File.ReadLines("C:/Users/larsb/Documents/GitHub/AdventOfCode2022/AdventOfCode/input/calories.txt"))
+            {
+                index++;
+                if (line != "")
+                {
+                    currentCalories = currentCalories + int.Parse(line);
+                }
+
+                if (line == "" && currentCalories > topCalories)
+                {
+                    Console.WriteLine("nieuwe top calorie : " + currentCalories);
+                    topCalories = currentCalories;
+                    currentCalories = 0;
+                }
+                else if (line == "" && topCalories == 0)
+                {
+                    topCalories = currentCalories;
+                    currentCalories = 0;
+                }
+                else if (line == "")
+                {
+                    currentCalories = 0;
+                }
+            }
+            Console.WriteLine(currentCalories);
+            Console.WriteLine("top calorie:" + topCalories);
+        }
+
+        */
+
+
+
+        List<int> caloriesSum = new List<int>();
+
+
+        public void fillList()
+        {
+            int calorieSum = 0;
+            foreach (string line in File.ReadLines("C:/Users/larsb/Documents/GitHub/AdventOfCode2022/AdventOfCode/input/calories.txt"))
+            {
+                if (line == "")
+                {
+                    caloriesSum.Add(calorieSum);
+                    calorieSum = 0;
+                }
+                else
+                {
+                    calorieSum = calorieSum + int.Parse(line);
+                }
+            }
+        }
+
+        public void partOne()
+        {
+            fillList();
+            var caloriesDesc = caloriesSum.OrderByDescending(calorieSum => calorieSum);
+            Console.WriteLine(caloriesDesc.Take(1).Sum());
+        }
+        public void partTwo()
+        {
+
+            var caloriesDesc3 = caloriesSum.OrderByDescending(calorieSum => calorieSum);
+            Console.WriteLine(caloriesDesc3.Take(3).Sum());
+        }
+
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode;
+
+Console.WriteLine("Hello, World!");
+
+Day1 day1 = new Day1();
+day1.partOne();
+day1.partTwo();
